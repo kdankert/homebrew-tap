924cd7a50419b08f95626443ad05657cad650d6d
Kjell Dankert <k.dankert@wtnet.de>
2025-07-12 01:47:13 -0700

    Update audiobookshelf to v2.25.1

--- Formula/a/audiobookshelf.rb
+++ Formula/a/audiobookshelf.rb
@@ -1,8 +1,8 @@
 class Audiobookshelf < Formula
   desc "Self-hosted audiobook and podcast server"
   homepage "https://audiobookshelf.org"
-  url "https://github.com/advplyr/audiobookshelf/archive/refs/tags/v2.20.0.tar.gz"
-  sha256 "b0a3072c3274658c8de35af66a1ddffb5fee33eb5ec293046edb26a865a2519e"
+  url "https://github.com/advplyr/audiobookshelf/archive/refs/tags/v2.25.1.tar.gz"
+  sha256 "4606d64ea2464cf93a9eedb156965b3ad76285300a1c8d6e6c458b5ace8c5f03"
   license "GPL-3.0-only"
 
   depends_on "ffmpeg"
@@ -10,21 +10,25 @@ class Audiobookshelf < Formula
   depends_on "python-setuptools"
 
   def install
-    system Formula["node"].libexec/"bin/npm", "ci", "--prefix", "client"
-    system Formula["node"].libexec/"bin/npm", "run", "--prefix", "client", "generate"
-    system Formula["node"].libexec/"bin/npm", "ci"
+    system "npm", "ci", "--prefix", "client"
+    system "npm", "run", "--prefix", "client", "generate"
+    system "npm", "ci"
     prefix.install Dir["*"]
+    (bin/"audiobookshelf").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["node"].opt_bin}/node" "#{opt_prefix}/index.js"
+    EOS
+    chmod 0555, bin/"audiobookshelf"
   end
 
   service do
-    run [Formula["node"].libexec/"bin/npm", "--prefix", opt_prefix, "start"]
+    run [opt_bin/"audiobookshelf"]
     keep_alive true
     error_log_path var/"log/audiobookshelf/server.log"
     log_path var/"log/audiobookshelf/server.log"
-    environment_variables PATH: std_service_path_env, CONFIG_PATH: etc/"audiobookshelf-config", METADATA_PATH: etc/"audiobookshelf-metadata", SOURCE: "brew"
-  end
-
-  test do
-    system "true"
+    environment_variables PATH:          std_service_path_env,
+                          CONFIG_PATH:   etc/"audiobookshelf-config",
+                          METADATA_PATH: etc/"audiobookshelf-metadata",
+                          SOURCE:        "brew"
   end
 end
